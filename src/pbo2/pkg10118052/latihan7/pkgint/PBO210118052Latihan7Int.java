c169b849f7b1c7104f73dc900905aa5231853284
Bagus Syibro Malisi <55564197+syibro98@users.noreply.github.com>
2019-09-24 12:44:19 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118052/latihan7/pkgint/PBO210118052Latihan7Int.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118052.latihan7.pkgint;
+
+/**
+ * @author 
+ * NAMA      : Bagus Syibro Malisi
+ * KELAS     : PBO2
+ * NIM       : 10118052
+ * Deskripsi Program : Program ini berisi program untuk menmapilkan 
+ * jenis-jenis tipe bilangan bulat
+ */
+public class PBO210118052Latihan7Int {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+        
+    }
+    
+}
